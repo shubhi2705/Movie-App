2d84309a0830088e6ccde753cf3f106e0823f852
Shubhi2705 <shubhisinghal2705@gmail.com>
2021-09-13 21:31:57 +0530

    first commit

---
+++ src/Key/Keys.js
@@ -0,0 +1,5 @@
+export const IMG_API="https://image.tmdb.org/t/p/w500/";
+export const FEATURED_API="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=822b219cd25e68c27f21852480a0698b&page=1"
+export const SEARCH_API="https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=822b219cd25e68c27f21852480a0698b&query="
+
+
